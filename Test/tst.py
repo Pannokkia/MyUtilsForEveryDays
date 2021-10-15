e7cc0f90545fecd8b4e21dec6502622198586cfd
unknown <luca.francesconi1977@outlook.com>
2021-10-15 22:13:47 +0200

    Added function that returns the number of lines within a file (blank lines are not excluded)

--- Test/tst.py
+++ Test/tst.py
@@ -2,7 +2,10 @@ import sys
 
 #Python check my module in following directory
 sys.path.insert(1, '/MyUtils')
-from MyUtilsForEveryDays.file import touch
+from MyUtilsForEveryDays.file import touch, lines_count
 
 res = touch('test1.txt','test2.txt')
 print(res)
+
+lines = lines_count('/MyUtils/MyUtilsForEveryDays/Test/file.txt')
+print(lines)
--- file.py
+++ file.py
@@ -9,14 +9,34 @@ def touch(*files) -> bool:
 
     try:
         for file in files:
-            open(file, 'w').close()
-        
+            open(file, 'w').close()       
         return True
-
     except FileNotFoundError as e:
         print('Check path exist!' , e.filename)
         return False
     except PermissionError as e:
         print('Permission denied!' , e.filename)
         return False
+
+def lines_count(file_to_read) -> int:
+    """
+    Read the number of lines in the file
     
+    Args:
+        file_to_read : path + filename 
+
+    Returns:
+        int: Returns the number of lines of which the file is composed
+    """
+    try:
+        lines = []
+        print(file_to_read)
+        f = open(file_to_read, "r")
+        lines = f.readlines()
+        return len(lines)
+    except FileNotFoundError as e:
+        print('Check file exsit', e.filename)
+        return -1
+    except PermissionError as e:
+        print('Permission denied!' , e.filename)
+        return False
