926ea686ad48e3deec54691eae045165a7d9753b
unknown <luca.francesconi1977@outlook.com>
2021-10-15 20:19:13 +0200

    Add utilities for files

---
+++ file.py
@@ -0,0 +1,23 @@
+from os import makedirs, remove, rmdir, scandir, PathLike, truncate
+from typing import Union, Optional, TextIO, Any, List, Tuple
+
+def touch(*files) -> bool:
+
+    """ 
+    Create several empty files.
+    :param files: The list of file paths to create.
+    :return: The list of created files.
+
+    """
+
+    try:
+        for file in files:
+            open(file, 'w').close()
+        
+        return True
+        
+    except FileNotFoundError as e:
+        print('Check path exist!' , e.filename)
+    except PermissionError as e:
+        print('Permission denied!' , e.filename)
+    
