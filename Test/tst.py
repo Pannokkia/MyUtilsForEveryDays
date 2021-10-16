34a206e3098e703daa8c1fdf8172bfe7e274d4e2
unknown <luca.francesconi1977@outlook.com>
2021-10-16 22:15:15 +0200

    Add example to use rename_excel_sheet

--- Test/tst.py
+++ Test/tst.py
@@ -3,7 +3,7 @@ import sys
 #Python check my module in following directory
 sys.path.insert(1, '/MyUtils')
 from MyUtilsForEveryDays.file import touch, count_lines_with_blank_lines, count_lines_without_blank_lines
-from MyUtilsForEveryDays.excel import get_max_rows,get_max_cols
+from MyUtilsForEveryDays.excel import get_max_rows,get_max_cols, rename_excel_sheet
 
 res = touch('test1.txt','test2.txt')
 print(res)
@@ -15,8 +15,11 @@ lines = count_lines_without_blank_lines('/MyUtils/MyUtilsForEveryDays/Test/file.
 print(lines)
 
 
-num_rows = get_max_rows('/MyUtils/MyUtilsForEveryDays/Test/Test.xlsx')
+num_rows = get_max_rows('/MyUtils/MyUtilsForEveryDays/Test/Test.xlsx','Pluto')
 print(num_rows)
 
-num_cols = get_max_cols('/MyUtils/MyUtilsForEveryDays/Test/Test.xlsx')
+num_cols = get_max_cols('/MyUtils/MyUtilsForEveryDays/Test/Test.xlsx','Pluto')
 print(num_cols)
+
+
+rename_excel_sheet('/MyUtils/MyUtilsForEveryDays/Test/Test.xlsx','Topolino','Pippo')
