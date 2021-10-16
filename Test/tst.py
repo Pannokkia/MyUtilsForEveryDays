73c54258efdd734cc51a35d6034785d3cdc4a88b
unknown <luca.francesconi1977@outlook.com>
2021-10-16 19:57:40 +0200

    Update tst.py with others test

--- Test/tst.py
+++ Test/tst.py
@@ -2,13 +2,21 @@ import sys
 
 #Python check my module in following directory
 sys.path.insert(1, '/MyUtils')
-from MyUtilsForEveryDays.file import touch, count_lines, count_lines_exclude_blank_lines
+from MyUtilsForEveryDays.file import touch, count_lines_with_blank_lines, count_lines_without_blank_lines
+from MyUtilsForEveryDays.excel import get_max_rows,get_max_cols
 
 res = touch('test1.txt','test2.txt')
 print(res)
 
-lines = count_lines('/MyUtils/MyUtilsForEveryDays/Test/file.txt')
+lines = count_lines_with_blank_lines('/MyUtils/MyUtilsForEveryDays/Test/file.txt')
 print(lines)
 
-lines = count_lines_exclude_blank_lines('/MyUtils/MyUtilsForEveryDays/Test/file.txt')
+lines = count_lines_without_blank_lines('/MyUtils/MyUtilsForEveryDays/Test/file.txt')
 print(lines)
+
+
+num_rows = get_max_rows('/MyUtils/MyUtilsForEveryDays/Test/Test.xlsx')
+print(num_rows)
+
+num_cols = get_max_cols('/MyUtils/MyUtilsForEveryDays/Test/Test.xlsx')
+print(num_cols)
