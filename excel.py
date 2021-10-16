14cfb5bb3cdfdd32e40743af5be7e482d3ff1ca3
unknown <luca.francesconi1977@outlook.com>
2021-10-16 22:13:54 +0200

    Add rename excel sheet

--- excel.py
+++ excel.py
@@ -1,9 +1,14 @@
 import openpyxl as xls
 
 
-def get_max_rows(excel_filename) -> int:
+def get_max_rows(excel_filename,sheet_name='Sheet1') -> int:
     """
         Count the maximum number of occupied rows
+
+    Arguments: 
+        
+      excel_filename: excel file to process
+      sheet_name: exchel sheet name to check.If sheet name not set, default is Sheet1
 
     Returns:
         int: Return mac number occupied rows
@@ -11,23 +16,57 @@ def get_max_rows(excel_filename) -> int:
 
     try:
         wrkbk = xls.load_workbook(excel_filename)
-        sh = wrkbk.active
-        return sh.max_row
-    except Exception as e:
-        print("Error on get_max_rows!", e)
+        wrkbk.active = wrkbk[sheet_name]
+        return wrkbk[sheet_name].max_row
+    except KeyError as e:
+        print("Error:", e)
+        return -1
+    except PermissionError as e:
+        print("Error:", e)
         return -1
 
-def get_max_cols(excel_filename) -> int:
+def get_max_cols(excel_filename,sheet_name='Sheet1') -> int:
     """
         Count the maximum number of occupied columns
+
+    Arguments: 
+        
+        excel_filename: excel file to process
+        sheet_name: exchel sheet name to check.If sheet name not set, default is Sheet1
 
     Returns:
         int: Return mac number occupied columns
     """
     try:
         wrkbk = xls.load_workbook(excel_filename)
-        sh = wrkbk.active
-        return sh.max_column
-    except Exception as e:
-        print("Error on get_max_cols!", e)
+        wrkbk.active = wrkbk[sheet_name]
+        return wrkbk[sheet_name].max_column
+    except KeyError as e:
+        print("Error:", e)
         return -1
+    except PermissionError as e:
+        print("Error:", e)
+        return -1
+        
+def rename_excel_sheet(excel_filename,old_sheet_name,new_sheet_name) -> None:
+    """
+        Rename Sheet on excel file
+
+    Arguments: 
+
+        excel_filename: excel file to process
+        old_excel_sheet_name: old name of ecel sheet
+        new_sheet_name: new name of excel file
+
+    Returns:
+        int: Return mac number occupied columns
+    """
+    try:
+        wrkbk = xls.load_workbook(excel_filename)
+        sh = wrkbk[old_sheet_name]
+        sh.title = new_sheet_name
+        wrkbk.save(excel_filename)
+    except KeyError as e:
+        print("Error:", e)
+    except PermissionError as e:
+        print("Error:", e)
