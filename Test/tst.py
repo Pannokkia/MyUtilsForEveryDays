7917fd18f64c30452a5cd6d2273ad21ef57e6b04
unknown <luca.francesconi1977@outlook.com>
2021-10-18 16:12:38 +0200

    Add test case for change_style_cells

--- Test/tst.py
+++ Test/tst.py
@@ -6,7 +6,7 @@ from openpyxl.styles.fonts import Font
 #Python check my module in following directory
 sys.path.insert(1, '/MyUtils')
 from MyUtilsForEveryDays.file import touch, count_lines_with_blank_lines, count_lines_without_blank_lines
-from MyUtilsForEveryDays.excel import get_max_rows,get_max_cols, rename_excel_sheet, remove_excel_sheet, change_font_excel_cells
+from MyUtilsForEveryDays.excel import get_max_rows,get_max_cols, rename_excel_sheet, remove_excel_sheet, change_style_cells
 
 COLOR_YELLOW = 'FFFF00'
 COLOR_BLACK = '000000'
@@ -34,4 +34,4 @@ COLOR_RED = 'FF0000'
 #remove_excel_sheet('/MyUtils/MyUtilsForEveryDays/Test/Test.xlsx','Popo')
 
 lst_cells_to_change_font_style = ['A1','A3','D6']
-change_font_excel_cells('/MyUtils/MyUtilsForEveryDays/Test/Test.xlsx',lst_cells_to_change_font_style, 'Pluto1', COLOR_YELLOW, bold=True,size=30, name='Arial')
+change_style_cells('/MyUtils/MyUtilsForEveryDays/Test/Test.xlsx',lst_cells_to_change_font_style, 'Pluto1', COLOR_YELLOW, bold=True,size=30, name='Arial')
