b8d7a7a83b93a0de343c854cc65f7f8e953a365d
unknown <luca.francesconi1977@outlook.com>
2021-10-18 16:13:22 +0200

    Add function  change_style_cells

--- excel.py
+++ excel.py
@@ -1,5 +1,6 @@
 import openpyxl as xls
-import typing
+from openpyxl.styles import colors, fonts
+from openpyxl.styles import Color
 
 def get_max_rows(excel_filename, sheet_name = 'Sheet1') -> int:
     """
@@ -66,7 +67,66 @@ def rename_excel_sheet(excel_filename, old_sheet_name, new_sheet_name) -> None:
         sh = wrkbk[old_sheet_name]
         sh.title = new_sheet_name
         wrkbk.save(excel_filename)
+        wrkbk.close()
     except KeyError as e:
         print("Error:", e)
     except PermissionError as e:
         print("Error:", e)
+
+def remove_excel_sheet(excel_filename, sheet_name_to_remove) -> None:
+    """
+        Remove Sheet from excel file
+
+    Arguments: 
+
+        excel_filename: excel file to process
+        sheet_name_to_remove: sheet to remove from excel file
+
+    Returns:
+        None
+    """
+    try:
+        wrkbk = xls.load_workbook(excel_filename)
+        wrkbk.remove(wrkbk[sheet_name_to_remove])
+        wrkbk.save(excel_filename)
+        wrkbk.close()
+    except KeyError as e:
+        print("Error:", e)
+    except PermissionError as e:
+        print("Error:", e)
+
+def change_style_cells(excel_filename, cells,  sheet_name = 'Sheet1', color="000000", bold = False, italic = False, strike = False, underline='none', size = 10, name='Century Gothic') -> None:
+    """
+    Sets the styles of  cells in an excel file 
+
+    Arguments: 
+
+        excel_filename: excel file to process
+        cells = list of cells for which to change the style
+        colors =list of cells for which to change the color
+        sheet_name = excel sheet where to set the new font style
+        b = Bold
+        i = Italic
+        strike = strike
+        umderline =  none, doubleAccounting, singleAccounting, double, single
+
+    Returns:
+        None
+    """
+    try:
+        wrkbk = xls.load_workbook(excel_filename)
+        wrkbk.active = wrkbk[sheet_name]
+        ws = wrkbk.active
+        for i in range(len(cells)):
+            ws[cells[i]].font = fonts.Font(b = bold, i = italic, strike = strike, u = underline,color=color, size=size, name=name)
+        wrkbk.save(excel_filename)
+        wrkbk.close()
+    except KeyError as e:
+        print("Error:", e)
+    except PermissionError as e:
+        print("Error:", e)
+    except IndexError as e:
+        print("Error:", e)
+    except ValueError as e:
+        print("Error:", e)
+      
