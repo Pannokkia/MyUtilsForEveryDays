cc406b46f645ef03ba2eaa076e4c6be48bb0d189
unknown <luca.francesconi1977@outlook.com>
2021-10-18 16:11:08 +0200

    Add test case for change_style_cells

--- Test/tst.py
+++ Test/tst.py
@@ -1,25 +1,37 @@
 import sys
+from openpyxl.styles import fonts
+
+from openpyxl.styles.fonts import Font
 
 #Python check my module in following directory
 sys.path.insert(1, '/MyUtils')
 from MyUtilsForEveryDays.file import touch, count_lines_with_blank_lines, count_lines_without_blank_lines
-from MyUtilsForEveryDays.excel import get_max_rows,get_max_cols, rename_excel_sheet
+from MyUtilsForEveryDays.excel import get_max_rows,get_max_cols, rename_excel_sheet, remove_excel_sheet, change_font_excel_cells
 
-res = touch('test1.txt','test2.txt')
-print(res)
+COLOR_YELLOW = 'FFFF00'
+COLOR_BLACK = '000000'
+COLOR_RED = 'FF0000'
 
-lines = count_lines_with_blank_lines('/MyUtils/MyUtilsForEveryDays/Test/file.txt')
-print(lines)
+#res = touch('test1.txt','test2.txt')
+#print(res)
 
-lines = count_lines_without_blank_lines('/MyUtils/MyUtilsForEveryDays/Test/file.txt')
-print(lines)
+#lines = count_lines_with_blank_lines('/MyUtils/MyUtilsForEveryDays/Test/file.txt')
+#print(lines)
 
-
-num_rows = get_max_rows('/MyUtils/MyUtilsForEveryDays/Test/Test.xlsx','Pluto')
-print(num_rows)
-
-num_cols = get_max_cols('/MyUtils/MyUtilsForEveryDays/Test/Test.xlsx','Pluto')
-print(num_cols)
+#lines = count_lines_without_blank_lines('/MyUtils/MyUtilsForEveryDays/Test/file.txt')
+#print(lines)
 
 
-rename_excel_sheet('/MyUtils/MyUtilsForEveryDays/Test/Test.xlsx','Pippo','Pluto')
+#num_rows = get_max_rows('/MyUtils/MyUtilsForEveryDays/Test/Test.xlsx','Pluto')
+#print(num_rows)
+
+#num_cols = get_max_cols('/MyUtils/MyUtilsForEveryDays/Test/Test.xlsx','Pluto')
+#print(num_cols)
+
+
+#rename_excel_sheet('/MyUtils/MyUtilsForEveryDays/Test/Test.xlsx','Pippo','Pluto')
+
+#remove_excel_sheet('/MyUtils/MyUtilsForEveryDays/Test/Test.xlsx','Popo')
+
+lst_cells_to_change_font_style = ['A1','A3','D6']
+change_font_excel_cells('/MyUtils/MyUtilsForEveryDays/Test/Test.xlsx',lst_cells_to_change_font_style, 'Pluto1', COLOR_YELLOW, bold=True,size=30, name='Arial')
